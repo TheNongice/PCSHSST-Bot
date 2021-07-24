0c5f8824f805cc3a5580ef62f6fdd0b8ef1f8d44
Wasawat Junnasaksri <ice7533678@gmail.com>
2021-07-24 07:34:13 +0000

    แก้รายละเอียด Readme

--- start.js
+++ start.js
@@ -1,33 +1,27 @@
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
-// Import the discord.js module
+// Discord.JS API Base
 const Discord = require('discord.js');
-
-// Create an instance of a Discord client
 const client = new Discord.Client();
 const auth = require('./auth.json');
+
 const hello_pccshst = require("./msg/hello/msg.json");
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
+let prefix ='จภ.สตูล';
+// Status & Ready to services!
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('มาร์ช จภ.',{type:"LISTENING"});
  });
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
+  if (message.content === prefix +' สวัสดี') {
+    // Send "pong" to the same channel
+    message.channel.send(hello_pccshst.hello1);
+  }
+  if (message.content === prefix +' ขอเพลงมาร์ช') {
+    message.channel.send({files:["./music/march/marsh.mp3"]});
+  }
   if (message.content === 'สวัสดี จภ.สตูล') {
     // Send "pong" to the same channel
     message.channel.send(hello_pccshst.hello1);
   }
-  if (message.content === 'จภ.สตูล ขอเพลงมาร์ช') {
-    // Send "pong" to the same channel
-    message.channel.send({files:["./music/march/marsh.mp3"]});
-  }
 });
-client.login(auth.token);
+client.login(auth.token); // token auth
